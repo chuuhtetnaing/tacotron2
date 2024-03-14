e01a6ba26b41d427b7f6b3e95cece795e8760974
chuuhtetnaing <chuuhtetnaing@gmail.com>
2024-03-14 15:08:24 +0800

    updated the code to be compatible with newer libraries

--- hparams.py
+++ hparams.py
@@ -1,11 +1,11 @@
-import tensorflow as tf
+from box import Box
 from text import symbols
 
 
 def create_hparams(hparams_string=None, verbose=False):
     """Create model hyperparameters. Parse nondefault from given string."""
 
-    hparams = tf.contrib.training.HParams(
+    hparams = Box(
         ################################
         # Experiment Parameters        #
         ################################
